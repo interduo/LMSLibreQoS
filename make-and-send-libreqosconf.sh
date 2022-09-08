b450b50773e3986061dac065b75405f1f20e14f9
Interduo <jkl@interduo.pl>
2022-09-08 11:42:08 +0200

    Update make-and-send-libreqosconf.sh

--- make-and-send-libreqosconf.sh
+++ make-and-send-libreqosconf.sh
@@ -10,12 +10,13 @@ LIBRESSHPORT=22
 LIBREQOSHOST='libreqoshost'
 LIBREQOSUSER='root'
 LIBREQOSDIR='~/LibreQoS/v1.2/'
+LIBREQOSMAGICCOMMAND="cd ${LIBREQOSDIR}; python3 LibreQoS.py 2>lastlog_std_err 1>lastlog_std_out"
 
 #Generate LibreQoS ShapeDevices.csv file
-${LMSPATH}/plugins/LMSLibreQoS/lms-makelibreqosconf.php
+php ${LMSPATH}/plugins/LMSLibreQoS/lms-makelibreqosconf.php
 
 #Send config files to LibreQoS
 scp -P${LIBRESSHPORT} "${QOSFILE}" ${LIBREQOSUSER}@${LIBREQOSHOST}:"${LIBREQOSDIR}"
 
 #Spell a magic command in LibreQoS
-ssh -p${LIBRESSHPORT} ${LIBREQOSUSER}@${LIBREQOSHOST} ${LIBREQOSMAGICCOMMAND}
+ssh -p${LIBRESSHPORT} ${LIBREQOSUSER}@${LIBREQOSHOST} "${LIBREQOSMAGICCOMMAND}"
